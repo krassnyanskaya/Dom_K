be3755a5706463864d7b1743179f3e65c5b0b593
krassnyanskaya <118731628+krassnyanskaya@users.noreply.github.com>
2022-12-13 12:27:47 +0300

    Add files via upload

---
+++ дкр2.1.pas
@@ -0,0 +1,19 @@
+﻿var i, n, mysign, nsign: integer;
+   ismon: boolean;
+   a: array of integer;
+begin
+ Write('Введите размер массива: '); ReadLn(n);
+ SetLength(a, n);
+ WriteLn('Введите Элементы массива: ');
+ for i := 0 to n - 1 do ReadLn(a[i]);
+ mysign := sign(a[1] - a[0]);
+ if mysign >= 0 then mysign := 1;
+ ismon := true;
+ for i := 2 to n - 1 do
+ begin
+   nsign := sign(a[i] - a[i - 1]);
+   if nsign >= 0 then nsign := 1;
+   if nsign <> mysign then ismon := false;
+ end;
+ if ismon then WriteLn('Массив монотонный') else WriteLn('Массив не монотонный'); 
+end.
---
+++ дкр2.2.pas
@@ -0,0 +1,34 @@
+﻿var i, n, mysign, nsign: integer;
+   ismon: boolean;
+   a: array of integer;
+const
+  alfabet = ['а','б','в','г','д','е','ё','ж','з','и','й','к','л','м','н','о','п','р','с','т','у','ф','х','ц','ч','ш','щ','ъ','ы','ь','э','ю','я'];
+var
+  strn: string;
+  sim: integer;
+  w: integer;
+  flagletter: boolean;
+begin
+  writeln('Введите строку: ');
+  Readln(strn);
+  strn:= strn + ' ';
+  sim:= 0; 
+  w:= 0;
+  flagletter:= false;
+  for i:= 1 to Length(strn) do
+    begin
+      if strn[i] in alfabet then
+        begin
+          inc(sim);
+          flagletter:= true;
+        end;
+         if (strn[i] = ' ') and flagletter then
+           begin
+             inc(w);
+             flagletter:= false;
+           end;
+        end;
+  writeln('Количество букв в строке: ',sim);
+  writeln('Количество слов в строке: ',w);
+  readln;
+end.
